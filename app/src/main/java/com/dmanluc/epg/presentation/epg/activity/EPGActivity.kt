f50b031751b3cfbf2a6521b1efbd2c16aaeca3d1
Daniel Manrique Lucas <dmanluc91@gmail.com>
2018-07-31 00:05:03 +0200

    Add presentation layer with initial custom EPG widget
    - Add resources files (colors, dimens, attrs)

--- app/src/main/java/com/dmanluc/epg/presentation/epg/activity/EPGActivity.kt
+++ app/src/main/java/com/dmanluc/epg/presentation/epg/activity/EPGActivity.kt
@@ -1,7 +1,8 @@
-package com.dmanluc.epg
+package com.dmanluc.epg.presentation.epg.activity
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.dmanluc.epg.R
 
 class EPGActivity : AppCompatActivity() {
 
