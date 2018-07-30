b15d7f14443bac2d82d8a51a8a8619885c93b16f
Daniel Manrique Lucas <dmanluc91@gmail.com>
2018-07-30 22:11:35 +0200

    Add initial project files

---
+++ app/src/main/java/com/dmanluc/epg/EPGActivity.kt
@@ -0,0 +1,12 @@
+package com.dmanluc.epg
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class EPGActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_epg)
+    }
+}
