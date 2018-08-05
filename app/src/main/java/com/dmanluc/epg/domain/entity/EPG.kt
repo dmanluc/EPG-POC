7a159536d780d3d33c96f5cb94e94ec95e28f7a0
Daniel Manrique Lucas <dmanluc91@gmail.com>
2018-08-05 16:40:48 +0200

    Add parcel feature to domain model

--- app/src/main/java/com/dmanluc/epg/domain/entity/EPG.kt
+++ app/src/main/java/com/dmanluc/epg/domain/entity/EPG.kt
@@ -1,24 +1,30 @@
 package com.dmanluc.epg.domain.entity
 
 import com.dmanluc.epg.app.toTimeFormat
+import org.parceler.Parcel
+import org.parceler.ParcelConstructor
 
 /**
  * @author   Daniel Manrique <dmanluc91@gmail.com>
  * @version  1
  * @since    1/8/18.
  */
+@Parcel(Parcel.Serialization.BEAN)
 data class EPG(val channels: List<Channel> = emptyList())
 
-data class Channel(val id: String,
-                   val title: String,
-                   val logoUrl: String,
-                   val schedules: List<Schedule>)
+@Parcel(Parcel.Serialization.BEAN)
+data class Channel @ParcelConstructor constructor(val id: String,
+                                                  val title: String,
+                                                  val logoUrl: String,
+                                                  val schedules: List<Schedule>)
 
-data class Schedule(val id: String,
-                     val title: String,
-                     val startTime: Long,
-                     val endTime: Long,
-                    val timeInPrettyFormat: String = "${startTime.toTimeFormat("HH:mm")} - ${endTime.toTimeFormat("HH:mm")}") {
+@Parcel(Parcel.Serialization.BEAN)
+data class Schedule @ParcelConstructor constructor(val id: String,
+                                                   val title: String,
+                                                   val startTime: Long,
+                                                   val endTime: Long,
+                                                   val timeInPrettyFormat: String = "${startTime.toTimeFormat(
+                                                           "HH:mm")} - ${endTime.toTimeFormat("HH:mm")}") {
 
     fun isLive() = System.currentTimeMillis() in startTime..endTime
 
